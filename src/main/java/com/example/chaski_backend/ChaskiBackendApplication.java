ad87fdfd414091419e901e84fc0251a5da2a9fcf
PieroH-del <pharryson1@alumno.uned.es>
2025-12-14 14:15:02 -0500

    Initial commit: Chaski Backend Spring Boot project

---
+++ src/main/java/com/example/chaski_backend/ChaskiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chaski_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaskiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChaskiBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chaski_backend/ChaskiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chaski_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaskiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
